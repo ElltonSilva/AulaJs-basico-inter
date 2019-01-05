3a11890971bf92267075e5e6c23af959e4199505
Elton Silva <ektondesantana@gmail.com>
2019-01-05 02:02:25 -0300

    Adicionando a primeira funcao no arquivo script.js e modoficando o index

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-var nome = "Elton";
+function trocarDiv(nome, idade){
+	var trocar = document.getElementById("trocar");
+	var texto = prompt("Qual seu sobrenome?");
+
+	trocar.innerHTML = nome+" "+texto+" tem "+idade+" anos " ;
+}
