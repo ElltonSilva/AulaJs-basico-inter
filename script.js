f18cd97a5b7c4f982c29364a3a633fcf728f16a2
Elton Silva <ektondesantana@gmail.com>
2019-01-06 16:12:01 -0300

    Aula de operacao

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ function addIng(){
 
 	document.getElementById("lista").innerHTML = listaHTML;
 }
+
+
+function somar(){
+	var campo1 = parseInt (document.getElementById ("campo1").value);
+	var campo2 = parseInt (document.getElementById ("campo2").value);
+
+	soma = campo1 + campo2;
+
+	alert (soma);
+
+}
